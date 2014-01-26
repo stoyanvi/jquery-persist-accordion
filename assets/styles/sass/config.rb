1e850f763f8599a6f810aff50ccd729d36ffbd3a
stoyanvi <stoyan.vi@gmail.com>
2014-01-26 15:18:33 +0200

    Updated gh-pages.

--- assets/styles/sass/config.rb
+++
@@ -1,9 +0,0 @@
-require 'compass-normalize'
-require 'canvas'
-
-sass_dir        = ''
-css_dir         = '../'
-images_dir      = '../../images'
-output_style    = :compressed
-relative_assets = true
-line_comments   = false
